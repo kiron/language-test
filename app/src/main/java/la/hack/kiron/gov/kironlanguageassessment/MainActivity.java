f5f224efa78046054b477c91189fc551b1306d1c
Julian Schamper <julian.schamper@rwth-aachen.de>
2016-04-29 22:05:48 +0200

    Add Start button

--- app/src/main/java/la/hack/kiron/gov/kironlanguageassessment/MainActivity.java
+++ app/src/main/java/la/hack/kiron/gov/kironlanguageassessment/MainActivity.java
@@ -1,5 +1,6 @@
 package la.hack.kiron.gov.kironlanguageassessment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button button = (Button) findViewById(R.id.start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello toast!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/la/hack/kiron/gov/kironlanguageassessment/QuestionActivity.java
@@ -0,0 +1,13 @@
+package la.hack.kiron.gov.kironlanguageassessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}

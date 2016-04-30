bdbc4e46a40a47912d12a73ebdf664b0ef634a9f
Christian Plewnia <christian.plewnia@rwth-aachen.de>
2016-04-30 03:45:46 +0200

    Timer service

--- app/src/main/java/la/hack/kiron/gov/kironlanguageassessment/QuestionActivity.java
+++ app/src/main/java/la/hack/kiron/gov/kironlanguageassessment/QuestionActivity.java
@@ -1,8 +1,13 @@
 package la.hack.kiron.gov.kironlanguageassessment;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -19,6 +24,8 @@ import la.hack.kiron.gov.kironlanguageassessment.data.Question;
 import la.hack.kiron.gov.kironlanguageassessment.data.Test;
 
 public class QuestionActivity extends AppCompatActivity {
+
+    private static final String TAG = QuestionActivity.class.getSimpleName();
 
     private static final int TEST_TIME = 50; /* minutes */
 
@@ -55,16 +62,16 @@ public class QuestionActivity extends AppCompatActivity {
 
         timerTextView = (TextView)findViewById(R.id.timer);
 
-        new CountDownTimer(TEST_TIME * 60 * 1000, 1000) {
-
-            public void onTick(long millisUntilFinished) {
-                timerTextView.setText(millisUntilFinished / 1000 / 60 + ":" + millisUntilFinished / 1000 % 60);
-            }
-
-            public void onFinish() {
-                timerTextView.setText("00:00");
-            }
-        }.start();
+//        new CountDownTimer(TEST_TIME * 60 * 1000, 1000) {
+//
+//            public void onTick(long millisUntilFinished) {
+//                timerTextView.setText(millisUntilFinished / 1000 / 60 + ":" + millisUntilFinished / 1000 % 60);
+//            }
+//
+//            public void onFinish() {
+//                timerTextView.setText("00:00");
+//            }
+//        }.start();
 
         this.test = (Test)getIntent().getExtras().get("test");
 
@@ -100,6 +107,9 @@ public class QuestionActivity extends AppCompatActivity {
 
             }
         });
+
+        startService(new Intent(this, BroadcastService.class));
+        Log.i(TAG, "Started service");
     }
 
     private int getCheckedItemById(int checkedId) {
@@ -138,5 +148,50 @@ public class QuestionActivity extends AppCompatActivity {
 
         nextButton.setEnabled(index < questions.size()-1);
         previousButton.setEnabled(index > 0);
+    }
+
+    private BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateGUI(intent); // or whatever method used to update your GUI fields
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(br, new IntentFilter(BroadcastService.COUNTDOWN_BR));
+        Log.i(TAG, "Registered broacast receiver");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(br);
+        Log.i(TAG, "Unregistered broacast receiver");
+    }
+
+    @Override
+    public void onStop() {
+        try {
+            unregisterReceiver(br);
+        } catch (Exception e) {
+            // Receiver was probably already stopped in onPause()
+        }
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        stopService(new Intent(this, BroadcastService.class));
+        Log.i(TAG, "Stopped service");
+        super.onDestroy();
+    }
+
+    private void updateGUI(Intent intent) {
+        if (intent.getExtras() != null) {
+            long millisUntilFinished = intent.getLongExtra("countdown", 0);
+            //Log.i(TAG, "Countdown seconds remaining: " +  millisUntilFinished / 1000);
+            timerTextView.setText(millisUntilFinished / 1000 / 60 + ":" + millisUntilFinished / 1000 % 60);
+        }
     }
 }
